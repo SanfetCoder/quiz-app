3057fc015adf460e0a15958f24f93256fda7454e
Sanphet Songjindasak <sanphetsongjindasak@gmail.com>
2024-02-20 00:09:03 +0700

    Make border around <CategoryButton ?.

--- components/CategoryButton.js
+++ components/CategoryButton.js
@@ -1,9 +1,31 @@
-import { Button } from "react-native"
+import {StyleSheet, TouchableWithoutFeedback, Text, View} from "react-native"
 
 const CategoryButton = ({title}) => {
   return (
-    <Button title={title} />
+    <TouchableWithoutFeedback>
+      <View style={styles.border}>
+        <Text style={styles.content}>{title}</Text>
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
+
+const styles = StyleSheet.create({
+  border: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 50,
+    padding: 10,
+    minWidth : 50,
+    height: 40,
+    flexDirection : 'row',
+    justifyContent : 'center',
+    borderWidth : 1.5
+  },
+  content : {
+    fontSize : 14,
+    fontWeight : '500'
+  }
+})
 
 export default CategoryButton
--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,29 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import CategoryButton from "../components/CategoryButton";
 
 const Home = () => {
+  const categories = ["All","Science","Programming","Mathematics"];
   return (
     <SafeAreaView>
-      
+      <View style={styles.categoryList}>
+        {
+          categories.map((category)=>{
+            return <CategoryButton key={category} title={category}/>
+          })
+        }
+      </View>
     </SafeAreaView>
   )
 }
 
-
+const styles = StyleSheet.create({
+  categoryList : {
+    display : 'flex',
+    flexDirection : 'row',
+    columnGap : 10,
+    marginVertical : 10,
+    marginLeft : 10
+  }
+})
 
 export default Home;
