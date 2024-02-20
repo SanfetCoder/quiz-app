517ef4e0651983726de0d349f58a670b610d29f5
Sanphet Songjindasak <sanphetsongjindasak@gmail.com>
2024-02-20 16:07:47 +0700

    Able to navigate back to home page when in quiz app

--- screens/InQuiz.js
+++ screens/InQuiz.js
@@ -1,12 +1,64 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import { LinearGradient } from 'expo-linear-gradient';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useNavigation } from '@react-navigation/native';
 const InQuiz = () => {
   return (
-    <SafeAreaView>
-      <Text>This is in the quiz</Text>
-    </SafeAreaView>
+    <LinearGradient
+      colors={['#5224B7','#A248FF']}
+      >
+      <SafeAreaView
+        style={{
+          height : '100%',
+          width : '100%',
+          display : 'flex',
+          flexDirection : 'column',
+          alignItems : 'center'
+        }}>
+          <NavBar />
+      </SafeAreaView>
+    </LinearGradient>
+  )
+}
+
+const NavBar = () => {
+  const navigation = useNavigation();
+  return (
+    <View
+      style={{
+        width : '100%',
+        height : 50,
+        display : 'flex',
+        flexDirection : 'row',
+        justifyContent : 'space-between',
+        alignItems : 'center',
+        paddingHorizontal : 15
+      }}
+    >
+      <MaterialCommunityIcons 
+        onPress={()=>navigation.navigate('Home')}
+        style={{
+          color: 'white',
+          width : '50%'
+        }} 
+        size={30} 
+        name="close-circle"/>
+      <Text
+        style={{
+          color : 'white',
+          fontWeight : '600',
+          fontSize : 20,
+          height : 20,
+          marginRight : 'auto',
+          width : '50%',
+          transform : [{
+            translateX : -50
+          }]
+        }}
+      >React Quiz</Text>
+    </View>
   )
 }
 
